de1e371c4216bfa13637373b78ca87c689b0b6ab
marcoweb <marcocarvalho.web@gmail.com>
2023-04-24 19:12:58 +0000

    Spring

--- app/src/main/java/tarefasspring/controllers/TarefasController.java
+++ app/src/main/java/tarefasspring/controllers/TarefasController.java
@@ -1,3 +1,12 @@
+package tarefasspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class TarefasController {
-    
+    @RequestMapping("/")
+    public String list() {
+        return "tarefas.jsp"; 
+    }
 }
